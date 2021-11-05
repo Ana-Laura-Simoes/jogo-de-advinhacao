86692cc5222d2787efe66331290540fd6f6462d3
Ana-Laura-Simoes <ana.simoesmatos@gmail.com>
2021-11-05 16:42:42 -0300

    add while

--- advinhacao.py
+++ advinhacao.py
@@ -3,10 +3,31 @@ print("Bem vindo ao Jogo de Advinhação!")
 print("********************************")
 
 numero_secreto = 42
-numero_advinhado= int(input("Tente advinhar o número secreto:"))
+total_tentativas = 3
+tentativa=0
 
-if(numero_advinhado == numero_secreto):
-    print("UAU! Você acertou!!")
+while(tentativa <= total_tentativas):
 
-else:
-    print("Que pena! Você errou!!")
+    tentativa = tentativa + 1
+
+    print(f"Tentativa {tentativa} de {total_tentativas}")
+
+    numero_advinhado = int(input("Tente advinhar o número secreto:"))
+    acertou = numero_advinhado == numero_secreto
+    maior = numero_advinhado > numero_secreto
+    menor = numero_advinhado < numero_secreto
+
+    if (acertou):
+        print("UAU! Você acertou!!")
+        break
+
+    elif (maior):
+        print("Você errou! Seu chute foi maior do que o número secreto!")
+
+    elif (menor):
+        print("Você errou! Seu chute foi menor do que o número secreto!")
+
+    else:
+        print("Que pena! Você errou!!")
+
+print("Fim de Jogo!")
