95113088457047bd350541313d61705372bd6e11
Ana-Laura-Simoes <ana.simoesmatos@gmail.com>
2021-11-05 17:07:58 -0300

    input value validation

--- advinhacao.py
+++ advinhacao.py
@@ -6,13 +6,18 @@ numero_secreto = 42
 total_tentativas = 3
 tentativa=0
 
-while(tentativa <= total_tentativas):
+while(tentativa < total_tentativas):
 
     tentativa = tentativa + 1
 
     print(f"Tentativa {tentativa} de {total_tentativas}")
 
-    numero_advinhado = int(input("Tente advinhar o número secreto:"))
+    numero_advinhado = int(input("Digite um número entre 1 e 100:"))
+
+    if(numero_advinhado < 1 or numero_advinhado > 100):
+        print("Valor inválido!")
+        continue
+
     acertou = numero_advinhado == numero_secreto
     maior = numero_advinhado > numero_secreto
     menor = numero_advinhado < numero_secreto
