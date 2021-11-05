17a0e6eadc6cc197b82caf6e1ca7a0e7f95ce66f
Ana-Laura-Simoes <ana.simoesmatos@gmail.com>
2021-11-05 16:01:58 -0300

    inicial code

--- advinhacao.py
+++ advinhacao.py
@@ -1,3 +1,12 @@
 print("********************************")
 print("Bem vindo ao Jogo de Advinhação!")
 print("********************************")
+
+numero_secreto = 42
+numero_advinhado= int(input("Tente advinhar o número secreto:"))
+
+if(numero_advinhado == numero_secreto):
+    print("UAU! Você acertou!!")
+
+else:
+    print("Que pena! Você errou!!")
