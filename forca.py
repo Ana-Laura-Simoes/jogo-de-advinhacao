d36e48a51b33f2432b30a524b3b2e0e7c44f08e8
Ana-Laura-Simoes <ana.simoesmatos@gmail.com>
2021-11-11 10:25:01 -0300

    forca game

--- forca.py
+++ forca.py
@@ -4,6 +4,28 @@ def jogar():
     print("Bem vindo ao Jogo da Forca!")
     print("********************************")
 
+    palavra_secreta = "banana"
+    enforcou = False
+    acertou= False
+    letras_acertadas = []
+
+    for letra in palavra_secreta:
+        letras_acertadas.append(" _ ")
+
+    while (not enforcou and not acertou):
+        chute = input("Qual letra? ")
+        #tirando os espaços do chute(se tiver)
+        chute = chute.strip()
+
+        index = 0
+        for letra in palavra_secreta:
+            if (chute.upper() == letra.upper()):
+                letras_acertadas[index] = letra
+            index = index + 1
+        print(letras_acertadas)
+
+
+
 
 if (__name__ == "__main__"):
     jogar()
